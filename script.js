18986c36f692a5e94a5474553548f60676864d2b
opti-mus <71773616+opti-mus@users.noreply.github.com>
2021-02-18 15:51:58 +0200

    update

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-const wrapper = document.querySelector('.side-menu-wrapper');
+// const wrapper = document.querySelector('.side-menu-wrapper');
 
-document.querySelector('.bars').addEventListener('click', () => {
-    wrapper.classList.add('anim');
-    document.querySelector('body').style.overflowY = 'hidden';
-})
+// document.querySelector('.bars').addEventListener('click', () => {
+//     wrapper.classList.add('anim');
+//     document.querySelector('body').style.overflowY = 'hidden';
+// })
 
-document.addEventListener('click', (e) => {
-    if (e.target.dataset.modal) {
-        wrapper.classList.remove('anim');
-        document.querySelector('body').style.overflowY = 'visible';
-    }
-})
+// document.addEventListener('click', (e) => {
+//     if (e.target.dataset.modal) {
+//         wrapper.classList.remove('anim');
+//         document.querySelector('body').style.overflowY = 'visible';
+//     }
+// })
 
-console.log(document.querySelector('.header-nav ul').children);
+// console.log(document.querySelector('.header-nav ul').children);
 
-function fillMenu() {
-    const menu = document.querySelector('#side-menu');
-    const nav = document.querySelector('.header-nav ul').children;
+// function fillMenu() {
+//     const menu = document.querySelector('#side-menu');
+//     const nav = document.querySelector('.header-nav ul').children;
     
-    for (let i = 0; i < nav.length; i++) {
-        menu.innerHTML += nav[i].outerHTML
-    }
+//     for (let i = 0; i < nav.length; i++) {
+//         menu.innerHTML += nav[i].outerHTML
+//     }
 
-}
-fillMenu()
+// }
+// fillMenu()
 
 baguetteBox.run('.works-cards');
